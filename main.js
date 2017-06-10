dd8d37a837f64ef45b0cb9a5a5097d30bb6aa042
Adrian Charkiewicz <riraasaa@gmail.com>
2017-06-10 13:53:08 +0200

    Add files via upload
    exporting json.data from main.js file

--- main.js
+++ main.js
@@ -2,83 +2,12 @@ var app = angular.module("myApp", []);
 app.controller("myCtrl", function($scope,$http) {
 
 // dane tabeli
- $scope.columns = [ {
-      "column1":"Treść kolumny 1",
-        "column2":"Treść kolumny 2",
-       "column3":"Treść kolumny 3",
-        "column4":"Treść kolumny 4",
-        "id":"0"
-   },
-  {
-      "column1":"Treść kolumny 1",
-        "column2":"Treść kolumny 2",
-       "column3":"Treść kolumny 3",
-        "column4":"Treść kolumny 4",
-        "id":"1"
-   },
-   {
-      "column1":"Treść kolumny 1",
-        "column2":"Treść kolumny 2",
-       "column3":"Treść kolumny 3",
-        "column4":"Treść kolumny 4",
-        "id":"2"
-   },
-   {
-      "column1":"Treść kolumny 1",
-        "column2":"Treść kolumny 2",
-       "column3":"Treść kolumny 3",
-        "column4":"Treść kolumny 4",
-        "id":"3"
-   },
-   {
-      "column1":"Treść kolumny 1",
-        "column2":"Treść kolumny 2",
-       "column3":"Treść kolumny 3",
-        "column4":"Treść kolumny 4",
-        "id":"4"
-   },
-   {
-      "column1":"Treść kolumny 1",
-        "column2":"Treść kolumny 2",
-       "column3":"Treść kolumny 3",
-        "column4":"Treść kolumny 4",
-        "id":"5"
-   },
-   {
-      "column1":"Treść kolumny 1",
-        "column2":"Treść kolumny 2",
-       "column3":"Treść kolumny 3",
-        "column4":"Treść kolumny 4",
-        "id":"8"
-   },
-   {
-      "column1":"Treść kolumny 1",
-        "column2":"Treść kolumny 2",
-       "column3":"Treść kolumny 3",
-        "column4":"Treść kolumny 4",
-        "id":"9"
-   },
-   {
-      "column1":"Treść kolumny 1",
-        "column2":"Treść kolumny 2",
-       "column3":"Treść kolumny 3",
-        "column4":"Treść kolumny 4",
-        "id":"10"
-   },
-   {
-      "column1":"Treść kolumny 1",
-        "column2":"Treść kolumny 2",
-       "column3":"Treść kolumny 3",
-        "column4":"Treść kolumny 4",
-        "id":"11"
-   },
-   {
-      "column1":"Treść kolumny 1",
-        "column2":"Treść kolumny 2",
-       "column3":"Treść kolumny 3",
-        "column4":"Treść kolumny 4",
-        "id":"12"
-   }];              
+  $http.get("data.json").then(
+     function (response) 
+      { 
+        $scope.columns = response.data;
+        length=$scope.columns.length;
+      });
 
 
 //wyświetla tabelę
@@ -88,14 +17,13 @@ app.controller("myCtrl", function($scope,$http) {
 //ukrywa tabelę
     $scope.functionLogOut=function() 
     {
-		$scope.showTable=false;
+		    $scope.showTable=false;
     };
-
 
 //wybiera id klikniętego wiersza i pozwala na jego edycję, a poźniej zapis
     $scope.editingData = {};
     
-    for (var i = 0, length = $scope.columns.length; i < length; i++)
+    for (var i = 0; i < length; i++)
     {
       $scope.editingData[$scope.columns[i].id] = false;
     }
@@ -116,7 +44,7 @@ app.controller("myCtrl", function($scope,$http) {
 	$scope.highlight =function(column)
 	{
         //podświetla wybrany wiersz
-	  $scope.selected=column.id;
+	      $scope.selected=column.id;
 	};
 
 
